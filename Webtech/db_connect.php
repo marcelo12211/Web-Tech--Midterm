1e2c974aa70f17099b54e13856d17bfd8f63658d
Jerald Fajardo <2235107@slu.edu.ph>
2025-12-09 23:04:30 +0800

    added documents, manage users, manage residents html

--- Webtech/db_connect.php
+++ Webtech/db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";        
 $password = "";         
-$dbname = "happyhalloww";
+$dbname = "happyhallow";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
