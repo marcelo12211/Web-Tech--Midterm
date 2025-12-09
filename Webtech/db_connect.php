6e69d3cf58f13ca26b6758e627c288f3de846b71
Jerald Fajardo <2235107@slu.edu.ph>
2025-12-09 23:18:15 +0800

    minor update

--- Webtech/db_connect.php
+++ Webtech/db_connect.php
@@ -2,12 +2,10 @@
 $servername = "localhost";
 $username = "root";        
 $password = "";         
-$dbname = "happyhallow";
+$dbname = "happyhalloww";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
